a72b5cc9c8048d8c36f6ed2badc79a9e42397c90
dev_sm <66045197+tmdals9974@users.noreply.github.com>
2021-08-24 14:55:41 +0900

    sitemap 추가

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -43,6 +43,6 @@ module.exports = {
 	},
 	plugins: [
 		'@vuepress/back-to-top',
-		// ['sitemap', { hostname: 'https://tmdals9974.github.io/TIL/' }]
+		['sitemap', { hostname: 'https://tmdals9974.github.io/TIL/' }]
 	]
 };
