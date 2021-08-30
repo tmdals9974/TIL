b3c39cf5d07928203a8b0e6870b084d0979b8af9
dev_sm <66045197+tmdals9974@users.noreply.github.com>
2021-08-30 17:27:59 +0900

    exif Update

--- docs/.vuepress/components/Exif.vue
+++ docs/.vuepress/components/Exif.vue
@@ -1,46 +1,51 @@
 <template>
 	<div>
 		<h1>Exif Test</h1>
-		<img
-			id="catImg"
-			src="https://tmdals9974.github.io/TIL/cat.jpg"
-			class="img-border"
-			alt="은하"
-		/>
-		<!-- <img id="catImg" src="/cat.jpg" class="img-border" alt="은하" /> -->
-		<button @click="getEXIF()">이미지 메타데이터 조회</button>
+
+		<h2>이미지 메타데이터 조회</h2>
+
+		<h3>이미지를 업로드하시면 해당 이미지의 메타데이터가 출력됩니다.</h3>
+		<input type="file" id="imgUpload" @change="getEXIF()" accept="image/*" />
+		
+		<h4 id="imageDescription"></h4>
 		<h4 id="exifContent"></h4>
 	</div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			btnClick: false,
-		};
-	},
 	methods: {
 		getEXIF() {
-			if (!this.btnClick) {
-				let script = document.createElement("script");
-				script.text = `
-				var imgElem = document.getElementById("catImg");
-				imgElem.exifdata = null; // 같은 페이지 안에서 이미지가 바뀌는 경우 이전의 메타데이터 정보를 지워주어야 함.
-				EXIF.getData(imgElem, function() {
-					var allMetaData = EXIF.getAllTags(this); //모든 EXIF정보
-					document.getElementById("exifContent").innerText = JSON.stringify(allMetaData, null, "\t");
+			if (document.getElementById("exifScript"))
+				document.head.removeChild(document.getElementById("exifScript"));
+
+			let script = document.createElement("script");
+			script.id = "exifScript";
+			script.text = `
+				var img = document.getElementById("imgUpload").files[0];
+				window.exifr.parse(img).then(exif => {
+					document.getElementById("imageDescription").innerText = '설명 : ' + exif.ImageDescription;
+					document.getElementById("exifContent").innerText = JSON.stringify(exif);
 				});
 			`;
-				document.head.appendChild(script);
-				this.btnClick = true;
-			}
-		}
+			document.head.appendChild(script);
+		},
 	},
 	beforeMount() {
 		let recaptchaScript = document.createElement("script");
-		recaptchaScript.setAttribute("src", "https://cdn.jsdelivr.net/npm/exif-js");
+		recaptchaScript.setAttribute(
+			"src",
+			"https://cdn.jsdelivr.net/npm/exifr/dist/lite.umd.js"
+		);
 		document.head.appendChild(recaptchaScript);
+	},
+	remove() {
+		// imgElem.exifdata = null; // 같은 페이지 안에서 이미지가 바뀌는 경우 이전의 메타데이터 정보를 지워주어야 함.
+		// 		EXIF.getData(imgElem, function() {
+		// 			var allMetaData = EXIF.getAllTags(this); //모든 EXIF정보
+		// 			document.getElementById("exifContent").innerText = JSON.stringify(allMetaData, null, "\t");
+		// 			console.dir(allMetaData);
+		// 		});
 	},
 };
 </script>
